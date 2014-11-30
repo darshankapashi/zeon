5991c1952007f4bcfba98a15ea000c51593b0166
sagarc <chordiasagar14@gmail.com>
2014-11-30 13:44:56 -0800

    implemented methods for 2-phase commit between leader and server

--- src/if/server.thrift
+++ src/if/server.thrift
@@ -20,8 +20,10 @@ service ServerTalk {
 
   // assumes routingInfo as ground truth, will be used by replicas not involved in split and merge
   void receiveRoutingInfo(1: leader.RoutingInfo rountingInfo) throws (1: ServerTalkException se),
+
   // prepare phase for 2-phase commit to update the routing infio
   // will be used by servers involved in split and merge
   i32 prepareRecvRoutingInfo(1: leader.RoutingInfo routingInfo) throws (1: ServerTalkException se),
+
   i32 commitRecvRoutingInfo(1:leader.RoutingInfo routingInfo) throws (1:ServerTalkException se), 
 }
--- src/server/ServerTalkHandler.cpp
+++ src/server/ServerTalkHandler.cpp
@@ -1,4 +1,5 @@
 #include "DataStore.h"
+#include "Node.h"
 #include "ServerTalkHandler.h"
 #include "StateObjects.h"
 #include "Structs.h"
@@ -42,21 +43,52 @@ void ServerTalkHandler::invalidate(const zeonid_t zid) {
   myDataStore->removePersistedData(zid);
 }
 
-void ServerTalkHandler::receiveRoutingInfo(const RoutingInfo& rountingInfo) {
-    // Your implementation goes here
-    printf("receiveRoutingInfo\n");
+void ServerTalkHandler::receiveRoutingInfo(const RoutingInfo& routingInfo) {
+  int retStatus = prepareRecvRoutingInfo(routingInfo);
+  if (retStatus == NodeMessage::PREPARED_RECV_ROUTING_INFO) {
+    retStatus = commitRecvRoutingInfo(routingInfo);
+  }
+  if (retStatus != NodeMessage::COMMIT_RECV_ROUTING_INFO) {
+    auto se = ServerTalkException();
+    se.what = NodeMessage::STALE_ROUTING_INFO;
+    se.why = "timestamp or version is stale";
+    throw se;
+  }
 }
 
 int32_t ServerTalkHandler::prepareRecvRoutingInfo(const RoutingInfo& routingInfo) {
-  // Your implementation goes here
   printf("prepareRecvRoutingInfo\n");
-  return 0;
+  // check if version for latest copy of routingInfo
+  // Copy this in tempStateObjects
+  // Atomically replace the routing info based on lock
+  lock_guard<mutex> tempObjectsLock(myNode->lockTempObjectsNode_);
+  if (routingInfo.version < myNode->routingInfo_.version) {
+    return NodeMessage::STALE_ROUTING_INFO; 
+  }
+  myNode->updateRoutingInfoTemp_ = routingInfo;
+  auto it = myNode->updateRoutingInfoTemp_.nodeRegionMap.find(
+  myNode->me_.nodeId.nid);
+  if (it != myNode->updateRoutingInfoTemp_.nodeRegionMap.end()) {
+    myNode->updateNodeInfoTemp_ = 
+      myNode->updateRoutingInfoTemp_.nodeRegionMap[myNode->me_.nodeId.nid]; 
+  }
+  return NodeMessage::PREPARED_RECV_ROUTING_INFO;
 }
 
 int32_t ServerTalkHandler::commitRecvRoutingInfo(const RoutingInfo& routingInfo) {
-  // Your implementation goes here
   printf("commitRecvRoutingInfo\n");
-  return 0;
+  // Check if updateRoutingInfoTemp_ corresponds to routingInfo
+  // Change the status and lock all objects for Node
+  lock_guard<mutex> updateObjectsLock(myNode->lockTempObjectsNode_);
+  if (myNode->updateRoutingInfoTemp_.version != 
+      routingInfo.version) {
+    return NodeMessage::ABORT_RECV_ROUTING_INFO;
+  }
+  lock_guard<mutex> lock(myNode->lockNode_);
+  myNode->setStatus(NodeStatus::UPDATING);  
+  myNode->me_ = myNode->updateNodeInfoTemp_; 
+  myNode->routingInfo_ = myNode->updateRoutingInfoTemp_;
+  myNode->setStatus(NodeStatus::ACTIVE);
+  return NodeMessage::COMMIT_RECV_ROUTING_INFO;
 }
-
 }
