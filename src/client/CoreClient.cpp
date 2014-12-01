9b90973d6be0b35fd9c5341de60f1844e2d8372c
Darshan Kapashi <darshankapashi@gmail.com>
2014-11-30 23:04:31 -0800

    A more usage CoreClient

--- src/client/CoreClient.cpp
+++ src/client/CoreClient.cpp
@@ -13,7 +13,8 @@ using namespace apache::thrift::transport;
 
 using namespace core;
 
-DEFINE_int32(port, 9090, "Server port to connect to");
+DEFINE_int32(port1, 8000, "Server1 port to connect to");
+DEFINE_int32(port2, 8001, "Server2 port to connect to");
 
 Point makePoint(int x, int y) {
   Point p;
@@ -30,45 +31,80 @@ Data makeData(int id, Point p, string value = "") {
   return d;
 }
 
+class Client {
+ public:
+  Client(string ip, int port) {
+    socket.reset(new TSocket(ip, port));
+    transport.reset(new TBufferedTransport(socket));
+    protocol.reset(new TBinaryProtocol(transport));
+    client = new PointStoreClient(protocol);  
+    transport->open();
+  }
+
+  ~Client() {
+    transport->close();
+    delete client;
+  }
+
+  PointStoreClient& get() {
+    return *client;
+  }
+
+  boost::shared_ptr<TTransport> socket;
+  boost::shared_ptr<TTransport> transport;
+  boost::shared_ptr<TProtocol> protocol;
+  PointStoreClient* client;
+};
+
 int main(int argc, char **argv) {
   google::ParseCommandLineFlags(&argc, &argv, true);
-  boost::shared_ptr<TTransport> socket(new TSocket("localhost", FLAGS_port));
-  boost::shared_ptr<TTransport> transport(new TBufferedTransport(socket));
-  boost::shared_ptr<TProtocol> protocol(new TBinaryProtocol(transport));
-  PointStoreClient client(protocol);
 
+  Client client1 ("localhost", FLAGS_port1);
+  Client client2 ("localhost", FLAGS_port2);
   try {
-    transport->open();
-
     // Create 3 points
     Data d1 = makeData(1, makePoint(10, 10), "Point1");
     Data d2 = makeData(2, makePoint(20, 20), "Point2");
     Data d3 = makeData(3, makePoint(10, 20), "Point3");
     try {
       printf("Creating d1\n");
-      client.createData(d1.id, d1.point, time(nullptr), d1.value);
+      client1.get().createData(d1.id, d1.point, time(nullptr), d1.value);
       printf("Creating d2\n");
-      client.createData(d2.id, d2.point, time(nullptr), d2.value);
+      client1.get().createData(d2.id, d2.point, time(nullptr), d2.value);
       printf("Creating d3\n");
-      client.createData(d3.id, d3.point, time(nullptr), d3.value);
+      client1.get().createData(d3.id, d3.point, time(nullptr), d3.value);
+    } catch (ZeonException const& ze) {
+      printf("Create failed: %d %s\n", ze.what, ze.why.c_str());
+    }
+
+    // Create 3 points
+    Data d4 = makeData(4, makePoint(110, 10), "Point4");
+    Data d5 = makeData(5, makePoint(120, 20), "Point5");
+    Data d6 = makeData(6, makePoint(110, 20), "Point6");
+    try {
+      printf("Creating d4\n");
+      client2.get().createData(d4.id, d4.point, time(nullptr), d4.value);
+      printf("Creating d5\n");
+      client2.get().createData(d5.id, d5.point, time(nullptr), d5.value);
+      printf("Creating d6\n");
+      client2.get().createData(d6.id, d6.point, time(nullptr), d6.value);
     } catch (ZeonException const& ze) {
       printf("Create failed: %d %s\n", ze.what, ze.why.c_str());
     }
 
     vector<Data> ret;
     printf("Getting 3 nearest points to (15,15)\n");
-    client.getNearestKByPoint(ret, makePoint(15, 15), 3);
+    client1.get().getNearestKByPoint(ret, makePoint(15, 15), 3);
     for (auto const& d: ret) {
       cout << "Recv: " << d.id << " (" << d.point.xCord << "," << d.point.yCord << ") " << d.value << "\n";
     }
 
     printf("Getting 1 nearest points to (15,15)\n");
-    client.getNearestKByPoint(ret, makePoint(15, 15), 1);
+    client1.get().getNearestKByPoint(ret, makePoint(15, 15), 1);
     for (auto const& d: ret) {
       cout << "Recv: " << d.id << " (" << d.point.xCord << "," << d.point.yCord << ") " << d.value << "\n";
     }
 
-    transport->close();
   } catch (ZeonException const& ze) {
     cout << "ZeonException: " << ze.what << " " << ze.why << endl;
   } catch (TException const& tx) {
