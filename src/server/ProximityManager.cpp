2c3853accb9fe29446cbdda03be4b34361c2486b
Darshan Kapashi <darshankapashi@gmail.com>
2014-11-30 22:37:48 -0800

    Implement inRegion funciton in proximity

--- src/server/ProximityManager.cpp
+++ src/server/ProximityManager.cpp
@@ -8,16 +8,16 @@ using namespace core;
 ProximityManager* proximity;
 
 void LinearProximityCompute::insertPoint(Data data) {
-  dataList_->emplace_back(data);
+  dataList_.emplace_back(data);
 }
 
 void LinearProximityCompute::removePoint(Data data) {
-  for (std::vector<Data>::iterator it =  dataList_->begin();
-       it != dataList_->end(); ++it) {
+  for (std::vector<Data>::iterator it =  dataList_.begin();
+       it != dataList_.end(); ++it) {
     if (it->id == data.id && 
         it->point.xCord == data.point.xCord && 
         it->point.yCord == data.point.yCord) {
-      dataList_->erase(it);
+      dataList_.erase(it);
     }
   }
 }
@@ -29,7 +29,7 @@ bool linearComparison(pair<double, Data> p1, pair<double, Data> p2) {
 vector<Data> LinearProximityCompute::getKNearestPoints(const Point& point, int k) {
   vector<pair<double, Data>> distanceVector;
   vector<Data> results;
-  for(auto d: *dataList_) {
+  for(auto d: dataList_) {
     cout<<"distance: "<<proximityDistance_->getDistance(
                       point, d.point)<<" id: "<<d.id;
     distanceVector.emplace_back(
@@ -41,8 +41,8 @@ vector<Data> LinearProximityCompute::getKNearestPoints(const Point& point, int k
     distanceVector.begin(), 
     distanceVector.end(), 
     linearComparison);
-  cout<<"first: "<<distanceVector[0].first<<" "<<distanceVector[0].second.id;
-  cout<<"second: "<<distanceVector[1].first<<" "<<distanceVector[1].second.id;
+  cout<<"first: "<<distanceVector[0].first<<" "<<distanceVector[0].second.id << "\n";
+  cout<<"second: "<<distanceVector[1].first<<" "<<distanceVector[1].second.id << "\n";
 
   // keep only k elements
   distanceVector.erase(
@@ -61,29 +61,9 @@ void LinearProximityCompute::getInternalPoints(vector<Data>& data, const Region&
 }
 
 void LinearProximityCompute::getInternalPoints(vector<Data>& data, const Rectangle& rectangle) {
-  // TODO: Implement this!
+  for (auto const& d: dataList_) {
+    if (inRectangle(rectangle, d.point)) {
+      data.push_back(d);
+    }
+  }
 }
-
-/*
-int main(int argc, char** argv) {
-  printf("Starting proximityManager");
-  auto config = ProximityManagerConfig();
-  config.algoType = LINEAR;
-  config.distanceType = EUCLIDEAN;
-  auto pManager = ProximityManager(config);
-  auto p1 = constructData(1,2,2,123,"first");
-  auto p2 = constructData(2,3,3,124, "second");
-  auto p3 = constructData(3,4,4,124, "second");
-  pManager.proximityCompute->insertPoint(p1);
-  pManager.proximityCompute->insertPoint(p2);
-  pManager.proximityCompute->insertPoint(p3);
-  auto point1 = Point();
-  point1.xCord = 1;
-  point1.yCord = 1;
-  auto res = pManager.proximityCompute->getKNearestPoints(point1, 3);
-  cout<<"res size: "<<res.size();
-  cout<<"p1: "<<res[0].point.xCord<<" "<<res[0].point.yCord;
-  cout<<"p2: "<<res[1].point.xCord<<" "<<res[1].point.yCord;
-  pManager.proximityCompute->removePoint(p1);
-}
-*/
--- src/server/Structs.cpp
+++ src/server/Structs.cpp
@@ -6,3 +6,12 @@ void throwError(ErrorCode what, string why) {
   ze.why = why;
   throw ze;
 }
+
+bool inRectangle(Rectangle const& r, Point const& p) {
+  if (p.xCord <= r.topRight.xCord && p.xCord >= r.bottomLeft.xCord &&
+      p.yCord <= r.topRight.yCord && p.yCord >= r.bottomLeft.yCord) {
+    return true;
+  } else {
+    return false;
+  }
+}
