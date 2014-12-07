56677d4dd048b80a58779a8af84aa33de8777cf4
Darshan Kapashi <darshankapashi@gmail.com>
2014-12-07 13:41:05 -0800

    Uncomment ClientTest.cpp

--- src/client/ClientTest.cpp
+++ src/client/ClientTest.cpp
@@ -14,18 +14,13 @@ using namespace apache::thrift::transport;
 
 using namespace core;
 
-DEFINE_int32(port1, 8000, "Server1 port to connect to");
-DEFINE_int32(port2, 8001, "Server2 port to connect to");
-
 DEFINE_bool(part1, true, "Run part 1");
 DEFINE_bool(part2, true, "Run part 2");
 
 int main(int argc, char **argv) {
   google::ParseCommandLineFlags(&argc, &argv, true);
 
-  /*
-  ZeonClient client1 ("localhost", FLAGS_port1);
-  ZeonClient client2 ("localhost", FLAGS_port2);
+  ZeonClient client;
   try {
     if (FLAGS_part1) {
       cout << "======= Create test" << endl;
@@ -35,11 +30,11 @@ int main(int argc, char **argv) {
       Data d3 = makeData(3, makePoint(10, 20), "Point3");
       try {
         printf("Creating d1\n");
-        client1.get().createData(d1.id, d1.point, time(nullptr), d1.value);
+        client.createData(d1.id, d1.point, time(nullptr), d1.value);
         printf("Creating d2\n");
-        client1.get().createData(d2.id, d2.point, time(nullptr), d2.value);
+        client.createData(d2.id, d2.point, time(nullptr), d2.value);
         printf("Creating d3\n");
-        client1.get().createData(d3.id, d3.point, time(nullptr), d3.value);
+        client.createData(d3.id, d3.point, time(nullptr), d3.value);
       } catch (ZeonException const& ze) {
         printf("Create failed: %d %s\n", ze.what, ze.why.c_str());
       }
@@ -50,11 +45,11 @@ int main(int argc, char **argv) {
       Data d6 = makeData(6, makePoint(180, 20), "Point6");
       try {
         printf("Creating d4\n");
-        client2.get().createData(d4.id, d4.point, time(nullptr), d4.value);
+        client.createData(d4.id, d4.point, time(nullptr), d4.value);
         printf("Creating d5\n");
-        client2.get().createData(d5.id, d5.point, time(nullptr), d5.value);
+        client.createData(d5.id, d5.point, time(nullptr), d5.value);
         printf("Creating d6\n");
-        client2.get().createData(d6.id, d6.point, time(nullptr), d6.value);
+        client.createData(d6.id, d6.point, time(nullptr), d6.value);
       } catch (ZeonException const& ze) {
         printf("Create failed: %d %s\n", ze.what, ze.why.c_str());
       }
@@ -62,13 +57,13 @@ int main(int argc, char **argv) {
       cout << "======= Get nearest K by point test" << endl;
       vector<Data> ret;
       printf("Getting 3 nearest points to (15,15)\n");
-      client1.get().getNearestKByPoint(ret, makePoint(15, 15), 3);
+      client.getNearestKByPoint(ret, makePoint(15, 15), 3);
       for (auto const& d: ret) {
         cout << "Recv: " << d.id << " (" << d.point.xCord << "," << d.point.yCord << ") " << d.value << "\n";
       }
 
       printf("Getting 1 nearest points to (15,15)\n");
-      client1.get().getNearestKByPoint(ret, makePoint(15, 15), 1);
+      client.getNearestKByPoint(ret, makePoint(15, 15), 1);
       for (auto const& d: ret) {
         cout << "Recv: " << d.id << " (" << d.point.xCord << "," << d.point.yCord << ") " << d.value << "\n";
       }
@@ -77,11 +72,11 @@ int main(int argc, char **argv) {
       try {
         Data dataToMove = makeData(1, makePoint(115, 10));
         dataToMove.prevPoint = makePoint(10, 10);
-        client2.get().setData(dataToMove, false);
+        client.setData(dataToMove, false);
 
         Data dataRecv;
         printf("Getting data from server1\n");
-        client1.get().getData(dataRecv, 1, true);
+        client.getData(dataRecv, 1, true);
         printData(dataRecv);
       } catch (ZeonException const& ze) {
         printf("Set failed: %d %s\n", ze.what, ze.why.c_str());
@@ -93,7 +88,7 @@ int main(int argc, char **argv) {
       try {
         Data data;
         cout << "Getting from server 1" << endl;
-        client1.get().getData(data, 4, true);
+        client.getData(data, 4, true);
         printData(data);
       } catch (ZeonException const& ze) {
         cout << "ZeonException: " << ze.what << " " << ze.why << endl;
@@ -102,7 +97,7 @@ int main(int argc, char **argv) {
       try {
         Data data;
         cout << "Getting from server 2" << endl;
-        client2.get().getData(data, 4, true);
+        client.getData(data, 4, true);
         printData(data);
       } catch (ZeonException const& ze) {
         cout << "ZeonException: " << ze.what << " " << ze.why << endl;
@@ -111,7 +106,7 @@ int main(int argc, char **argv) {
       try {
         cout << "Creating for point (110, 10)" << endl;
         Data d1 = makeData(1, makePoint(110, 10), "PointN");
-        client1.get().createData(d1.id, d1.point, time(nullptr), d1.value);
+        client.createData(d1.id, d1.point, time(nullptr), d1.value);
         cout << "PASS" << endl;
       } catch (ZeonException const& ze) {
         cout << "ZeonException: " << ze.what << " " << ze.why << endl;
@@ -120,7 +115,7 @@ int main(int argc, char **argv) {
       try {
         cout << "Creating for point (110, 10)" << endl;
         Data d1 = makeData(1, makePoint(110, 10), "PointN");
-        client2.get().createData(d1.id, d1.point, time(nullptr), d1.value);
+        client.createData(d1.id, d1.point, time(nullptr), d1.value);
       } catch (ZeonException const& ze) {
         cout << "PASS" << endl;
         cout << "ZeonException: " << ze.what << " " << ze.why << endl;
@@ -132,5 +127,4 @@ int main(int argc, char **argv) {
   } catch (TException const& tx) {
     cout << "ERROR: " << tx.what() << endl;
   }
-  */
 }
