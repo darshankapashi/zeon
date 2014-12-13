2f3aa455bfb12b3f716e657340eb6dd54bb82c76
sagarc <chordiasagar14@gmail.com>
2014-12-12 17:28:23 -0800

    Added plots refined over averaged data

--- scripts/plots/plot.sh
+++ scripts/plots/plot.sh
@@ -1,10 +1,10 @@
 set style line 1 lc rgb '#0060ad' lt 1 lw 2 pt 7 ps 1.5 
-set title "Performance with load balancing"
+set title "Performance with node failure"
 set ylabel "Number of queries"
 set xlabel "Time in sec"
 set xrange [0:80]
-set yrange [0:150]
+set yrange [0:100]
 set term png
-set output 'loadBalance.png'
-plot 'loadBalance.dat' with linespoint
+set output 'nodeFailure.png'
+plot 'failNode2.dat' with linespoint
 
